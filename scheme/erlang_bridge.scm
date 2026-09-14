409445a07c0f0dbbc821f53a1529169c7b0e7011
Daniel Araya P. <97203409+Daniel-AP@users.noreply.github.com>
2026-09-14 00:54:47 -0600

    feat: add scheme request bridge

---
+++ scheme/erlang_bridge.scm
@@ -0,0 +1,56 @@
+#lang swindle
+
+(require racket/file)
+
+(provide parse-request
+    write-success)
+
+;; Domain: Scheme command-line arguments
+;; Codomain: A valid request
+
+(define parse-request
+    (lambda (arguments)
+        (cond
+            ((and
+                (= (length arguments) 10)
+                (string=? (cadddr arguments) "gaussian"))
+                (list
+                    (car arguments)
+                    (cadr arguments)
+                    (caddr arguments)
+                    (list
+                        'gaussian
+                        (string->number (car (cddddr arguments)))
+                        (string->number (cadr (cddddr arguments))))
+                    (list
+                        (string->number (caddr (cddddr arguments)))
+                        (string->number (cadddr (cddddr arguments))))
+                    (list
+                        (string->number (car (cddddr (cddddr arguments))))
+                        (string->number (cadr (cddddr (cddddr arguments)))))))
+            ((and
+                (= (length arguments) 8)
+                (string=? (cadddr arguments) "grayscale"))
+                (list
+                    (car arguments)
+                    (cadr arguments)
+                    (caddr arguments)
+                    (list 'grayscale)
+                    (list
+                        (string->number (car (cddddr arguments)))
+                        (string->number (cadr (cddddr arguments))))
+                    (list
+                        (string->number (caddr (cddddr arguments)))
+                        (string->number (cadddr (cddddr arguments))))))
+            (else
+                (error "Invalid Scheme request")))))
+
+;; Domain: A status file path
+;; Codomain: No return
+
+(define write-success
+    (lambda (status-path)
+        (display-to-file
+            "ok"
+            status-path
+            #:exists 'replace)))
